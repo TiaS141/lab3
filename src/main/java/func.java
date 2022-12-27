7dd06d9a818504dd6712e389c79ad77413c667b3
Кочетов Матвей <tias999@yandex.ru>
2022-12-27 21:02:00 +0300

    add function in func.java

--- src/main/java/func.java
+++ src/main/java/func.java
@@ -1,2 +1,15 @@
 public class func {
+    // y=arcsin(-x) + 13x
+    private static double x;
+
+    public func(double x) {
+        this.x = x;
+    }
+    public static double function() {
+        double y = 0;
+        x = Math.toRadians(x);
+        y = Math.toDegrees(Math.asin(-x)) + 13*x;
+
+        return y;
+    }
 }
